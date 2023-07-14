f19c3b6060ac8abc949c6168ec88157a3b723e75
Dike-Code <dikeclintonobinna58@gmail.com>
2023-07-14 03:33:27 +0100

    Update-All

--- script/swift.js
+++ script/swift.js
@@ -1,8 +1,31 @@
+/** @format */
+
+const logo = document.querySelector('#brand-logo');
 const menu = document.querySelector('.menu');
-const nav = document.querySelector('.nav-sections')
+const nav = document.querySelector('.navbar');
+const navItems = document.querySelector('.nav-sections');
 
-menu.addEventListener('click', showMenu);
+// Event-trigger
+// Applied Hoisting
+eventTrigger();
 
+function eventTrigger() {
+   menu.addEventListener('click', showMenu);
+   window.addEventListener('scroll', stick);
+}
+
+// Stick Navbar
+function stick() {
+   if (window.scrollY > 600) {
+      nav.style = `background:#4d5060;`;
+      logo.style = `color:#efefef;`;
+   } else if (window.scrollY < 500) {
+      nav.style.backgroundColor = 'transparent';
+      logo.style = `color:#282d42`;
+   }
+}
+
+// Hamburger Handler
 function showMenu() {
-  nav.classList.toggle('show')
+   navItems.classList.toggle('show');
 }
